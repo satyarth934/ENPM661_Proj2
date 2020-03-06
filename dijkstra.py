db30c1ad80fedb525da683d6123153dc2c052d31
Satyarth Praveen <satyarth934@gmail.com>
2020-03-06 00:07:13 +0000

    Updating the dijkstra algorithm.

--- dijkstra.py
+++ dijkstra.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import copy
 import numpy as np
 import heapq
 
@@ -12,14 +13,24 @@ import node
 import utils
 
 
-def function(input_map):
+##
+## Gets the dijkstra path. 
+## In this algorithm, the heuristic cost from 
+## current node to goal node is not considered.
+##
+## :param      input_map:  The input map
+## :type       input_map:  { type_description }
+##
+def getDijkstraPath(input_map):
+	viz_map = copy.deepcopy(input_map)
+
 	start_r, start_c = 10, 10
-	goal_r, goal_c = 14, 14
-	start_node = node.Node((start_r, start_c), None, 0, utils.euclideanDistance((start_r, start_c), (goal_r, goal_c)))
-	goal_node = node.Node((goal_r, goal_c), None, -1, 0)
+	goal_r, goal_c = 77, 99
+	start_node = node.Node((start_r, start_c), None, 0, None)
+	goal_node = node.Node((goal_r, goal_c), None, -1, None)
 
 	# Saving a tuple with total cost and the state node
-	minheap = [((start_node.movement_cost + start_node.goal_cost), start_node)]
+	minheap = [((start_node.movement_cost), start_node)]
 	heapq.heapify(minheap)
 
 	# dictionary of all the visited nodes
@@ -41,7 +52,7 @@ def function(input_map):
 		if curr_node.shallowMatch(goal_node):
 			print("Reached Goal!")
 			# backtrack to get the path
-			utils.backtrack(curr_node, visited_nodes)
+			# utils.backtrack(curr_node, visited_nodes)
 			break
 		
 		# if hit an obstacle, ignore this movement
@@ -50,31 +61,30 @@ def function(input_map):
 
 		for row_step, col_step in movement_steps:
 			# Action Move
-			next_node = actions.actionMove(curr_node, (goal_r, goal_c), row_step, col_step)
+			next_node = actions.actionMove(curr_node, row_step, col_step)
+			
+			# ERROR <- GETTING NEGATIVE COORDINATES
+			# if next_node.current_coords[0] < 0 or next_node.current_coords[1] < 0:
+			# 	print next_node.current_coords
 			
 			if next_node is not None:
 				# Check if the current node has already been visited.
 				# If it has, then see if the current path is better than the previous one
 				# based on the total cost = movement cost + goal cost
-				
 				if next_node.current_coords in visited_nodes:
-					# if (next_node.movement_cost + next_node.goal_cost) < (visited_nodes[idx].movement_cost + visited_nodes[idx].goal_cost):
-					if (next_node.movement_cost + next_node.goal_cost) < (visited_nodes[next_node.current_coords].movement_cost + visited_nodes[next_node.current_coords].goal_cost):
+					if (next_node.movement_cost) < (visited_nodes[next_node.current_coords].movement_cost):
 						visited_nodes[next_node.current_coords].movement_cost = next_node.movement_cost
-						visited_nodes[next_node.current_coords].goal_cost = next_node.goal_cost
-						visited_nodes[next_node.current_coords].parent = next_node.parent
+						visited_nodes[next_node.current_coords].parent_coords = next_node.parent_coords
 
 						h_idx = utils.findInHeap(next_node, minheap)
 						if h_idx > -1:
-							minheap[idx][1].movement_cost = next_node.movement_cost
-							minheap[idx][1].goal_cost = next_node.goal_cost
-							minheap[idx][1].parent = next_node.parent
-
-							minheap[idx][0] = next_node.movement_cost + next_node.goal_cost
+							minheap[h_idx] = (next_node.movement_cost, next_node)							
 				else:
 					# visited_nodes.append(next_node)
 					visited_nodes[next_node.current_coords] = next_node
-					heapq.heappush(minheap, ((next_node.movement_cost+next_node.goal_cost), next_node))
+					heapq.heappush(minheap, ((next_node.movement_cost), next_node))
+
+					utils.drawOnMap(viz_map, next_node.current_coords, visualize=True)
 
 		heapq.heapify(minheap)
 
@@ -82,7 +92,7 @@ def function(input_map):
 
 def main():
 	input_map = obstacles.getMap()
-	function(input_map)
+	getDijkstraPath(input_map)
 
 
 if __name__ == '__main__':
